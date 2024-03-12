d867f074fbac99725aea0b307b7eff0f8ba7ed5f
coeat.k <coeat.k@kakaocorp.com>
2024-03-12 17:33:46 +0900

    feat: zoom 테스트 및 오키나와 표시

--- public/javascripts/google.map.js
+++ public/javascripts/google.map.js
@@ -15,15 +15,14 @@ loader.load()
 
 
 async function initMap() {
-    // The location of Uluru
-    const position = {lat: -25.344, lng: 131.031};
+    const position = {lat: 26.2188, lng: 127.6729};
     // Request needed libraries.
     //@ts-ignore
     const {Map} = await google.maps.importLibrary("maps");
     console.log(google);
     // The map, centered at Uluru
     map = new Map(document.getElementById("map"), {
-        zoom: 4,
+        zoom: 18,
         center: position,
         mapId: "DEMO_MAP_ID",
     });
